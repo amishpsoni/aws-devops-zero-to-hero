132a4e6d24f77bf33bad7d42926f6db23a03825a
amishpsoni <137361041+amishpsoni@users.noreply.github.com>
2024-07-19 18:47:16 +0000

    update

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,8 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    names = ["Amish","Ram","Harish","Abhishek"]
+    for name in names:
+        if name == "Amish":
+            print("Hello :" + name)
+            
+
     return 'Hello, world!'
 
 if __name__ == '__main__':
     app.run()
-
