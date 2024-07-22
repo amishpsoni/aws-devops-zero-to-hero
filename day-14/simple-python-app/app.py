75aa79944654e3c713419fb4bd56e00b19cd03c7
amishpsoni <137361041+amishpsoni@users.noreply.github.com>
2024-07-22 19:42:44 +0000

    update

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -1,16 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template_string
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    names = ["Amish","Ram","Harish","Abhishek"]
-    for name in names:
-        if name == "Amish":
-            print("Hello :" + name)
-            
-
-    return 'Hello, world!'
+def home():
+    html_content = """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Simple Flask Web Page</title>
+    </head>
+    <body>
+        <h1>Welcome to My Simple Flask Web Page</h1>
+        <p>This is a basic example of a web page served with Flask.</p>
+    </body>
+    </html>
+    """
+    return render_template_string(html_content)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
